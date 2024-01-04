6372253be03cc8a8005f6506012c34f4a013f2ba
Thiago Vicente <thiagovicente2@gmail.com>
2024-01-04 18:17:31 -0300

    new

--- script.js
+++ script.js
@@ -40,18 +40,28 @@ function calcularDistancia() {
     }
 }
 
+
+function atualizarValores() {
+    var kmporlitro = document.getElementById("kmporlitro").value;
+    var precoGasolina = document.getElementById("precoGasolina").value;
+
+
+    window.opener.atualizarValoresConfiguracao(kmporlitro, precoGasolina);
+
+    alert('Valores atualizados com sucesso!');
+}
+
+
 function calcularFrete() {
-    // Obtendo os valores do ponto de partida e destino
+
     var pontoPartida = document.getElementById("pontoPartida").value;
     var pontoDestino = document.getElementById("pontoDestino").value;
 
-    // Verificando se os pontos de partida e destino foram preenchidos
     if (pontoPartida === "" || pontoDestino === "") {
         alert('Por favor, preencha os pontos de partida e destino.');
         return;
     }
 
-    // Chamando a API do Google Maps para obter a distância
     var service = new google.maps.DistanceMatrixService();
     service.getDistanceMatrix(
         {
@@ -62,17 +72,15 @@ function calcularFrete() {
         },
         function (response, status) {
             if (status === 'OK') {
-                // Obtendo a distância real entre os pontos
-                var distanciaKm = response.rows[0].elements[0].distance.value / 1000; // Convertendo de metros para quilômetros
 
-                // Valores de referência
+                var distanciaKm = response.rows[0].elements[0].distance.value / 1000;
+
                 var kmPorLitro = 10;
                 var precoGasolina = 5.89;
 
-                // Calculando o custo do frete
                 var custoFrete = (distanciaKm / kmPorLitro) * precoGasolina;
 
-                // Exibindo a distância e o valor do frete nos campos de input
+
                 document.getElementById("valorFrete").value = `Distância: ${distanciaKm.toFixed(2)} km | Valor do Frete: ${custoFrete.toFixed(2)}`;
             } else {
                 alert('Erro ao calcular a distância: ' + status);
@@ -81,3 +89,11 @@ function calcularFrete() {
     );
 }
 
+function carregarConfiguracoes() {
+
+    var kmPorLitro = document.getElementById('kmporlitro').value;
+    var precoGasolina = document.getElementById('precoGasolina').value;
+
+    document.getElementById('kmPorLitroPrincipal').value = kmPorLitro;
+    document.getElementById('precoGasolinaPrincipal').value = precoGasolina;
+}
